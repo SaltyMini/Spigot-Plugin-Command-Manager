cf9d2aee1a602060bd7ab661584b8cfdf1ae1946
SaltyMini <samclaymichael@gmail.com>
2025-04-30 02:32:53 +0100

    Registered commands and added comments

--- src/main/java/com/ciderMC/spigotPluginCommandManager/SpigotPluginCommandManager.java
+++ src/main/java/com/ciderMC/spigotPluginCommandManager/SpigotPluginCommandManager.java
@@ -1,14 +1,19 @@
 package com.ciderMC.spigotPluginCommandManager;
 
+import com.ciderMC.spigotPluginCommandManager.commands.CommandManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class SpigotPluginCommandManager extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Sets the general command lable
         Objects.requireNonNull(getCommand("customcommands")).setExecutor(new CommandManager());
-        Objects.requireNonNull(getCommand("examplecommand")).setExecutor(new CommandManager());
 
+        // For using commandNames as a command lable
+        Objects.requireNonNull(getCommand("examplecommand")).setExecutor(new CommandManager());
     }
 
     @Override
